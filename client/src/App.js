d3e0db41a10f1ff822e7952d93487113213159b0
YowsifAhamed <yowsifahamed@gmail.com>
2021-09-11 23:39:36 +0530

    Project set up

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const App = () => {
+    return (<div>
+        <h1> Alhamdulillah </h1>
+    </div>)
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,4 @@
+import bodyParser from "body-parser";
+import Express from "express";
+import Mongoose from "mongoose";
+import cors from "cors"
