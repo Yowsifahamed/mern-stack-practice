b8f5d96d79e9dd3626ae84060c60d8a7ba1f18f8
YowsifAhamed <yowsifahamed@gmail.com>
2021-09-12 20:38:06 +0530

    controller, model added in server site

---
+++ server/controllers/post.js
@@ -0,0 +1,23 @@
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+    try {
+        const PostMessage = await PostMessage.find();
+        res.status(200).json(PostMessage);
+    } catch (error) {
+        res.status(400).json({ messgae: error.messgae })
+    }
+};
+
+
+export const createPost = async (req, res) => {
+    const post = res.body;
+    const newPost = await PostMessage(post);
+
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ messgae: error.messgae })
+    }
+};
