eba635e42994a7834547b549151e6b5db0a58758
Yowsif Ahamed <yowsifahamed@gmail.com>
2022-06-01 23:10:53 +0530

    inital commit

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -4,6 +4,7 @@ export const getPosts = async (req, res) => {
     try {
         const PostMessage = await PostMessage.find();
         res.status(200).json(PostMessage);
+        console.log(res.status(200).json(PostMessage), "res.status(200).json(PostMessage)")
     } catch (error) {
         res.status(400).json({ messgae: error.messgae })
     }
--- server/index.js
+++ server/index.js
@@ -1,4 +1,24 @@
-import bodyParser from "body-parser";
-import Express from "express";
-import Mongoose from "mongoose";
+import express from 'express'
+import bodyParser from 'body-parser';
+import mongoose from "mongoose";
 import cors from "cors"
+import postRoutes from './routes/post.js';
+
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()) // To parse the incoming requests with JSON payloads
+app.use(cors());
+
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_url = 'mongodb+srv://yowsif:fullstack12mm@cluster0.zzkam.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server runnung port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', true);
